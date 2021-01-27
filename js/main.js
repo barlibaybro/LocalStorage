0a3d43107b83578080059a81110201c9c537c26d
Zhansultan Chuakbayev <barlibaybro@gmail.com>
2021-01-28 00:56:01 +0600

    v1.3

--- js/main.js
+++ js/main.js
@@ -1,24 +1,31 @@
 
-function register() {
-	let login = document.getElementById('login').value;
+function signup() {
+	let username = document.getElementById('username').value;
 	let password = document.getElementById('password').value;
-	localStorage.setItem('login', login);
-	localStorage.setItem('password', password);
-	alert("Register successful");
+	localStorage.setItem(username + 'username', username);
+	localStorage.setItem(username + 'password', password);
+	alert("Sign up successfully, now you can sign in.");
 
 }
 
 function signin() {
-	let login = document.getElementById('login').value;
+	let username = document.getElementById('username').value;
 	let password = document.getElementById('password').value;
 
-	if ((login == localStorage.getItem('login')) && (password == localStorage.getItem('password'))) {
-		alert("Login successful");
+
+	if ((username == 'clearlocalStorage') && (password == 'admin')) {
+		localStorage.clear();
+		alert("localStorage cleared successfully!");
+	}
+
+	else if ((username == localStorage.getItem(username + 'username')) && (password == localStorage.getItem(username + 'password'))) {
+		alert("Sign in successfully!");
 	}
 
 	else {
-		alert("Incorrect login or password");
+		alert("Incorrect username or password. Please try again.");
 	}
 		
 }
+
 
